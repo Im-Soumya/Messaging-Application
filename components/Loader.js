7a0564add1979e85659e1ee20710f037049b006c
Soumya <aeie2019@rcciit.org.in>
2022-09-11 08:16:11 +0530

    updated ui

--- components/Loader.js
+++ components/Loader.js
@@ -1,7 +1,7 @@
 const Loader = () => {
   return (
     <div className="grid place-items-center h-screen text-4xl bg-white text-gray-700 font-semibold">
-      Loader to be added!
+      Loading..
     </div>
   );
 };
